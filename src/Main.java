9b0f412feccfa762498a947eb5d962c938d17723
Saleh Mahagney <saleh.mahagney@ibm.com>
2022-11-16 19:29:42 +0200

    Ch 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("args = " + Arrays.deepToString(args));
+        String a = "abc";
+        String b = "abc";
+        if(a==b){
+            System.out.println("a=b");
+        }
+
+        String c = new String("abc");
+        String d = new String("abc");
+        if(c==d){
+            System.out.println("c=d");
+        } else {
+            System.out.println("c!=d");
+
+        }
     }
 }
